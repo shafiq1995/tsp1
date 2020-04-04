519d720594d97f74a1754453ca0bfff126e0c97b
shafiq1995 <shafiqshumon0@gmail.com>
2020-04-05 01:32:19 +0600

    Second deplyment

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 
-app.get('/home', (req, res) => {
-    res.send({Hi: 'There'});
+app.get('/', (req, res) => {
+    res.send({Bye: 'There'});
 });
 
 // To deploy on heroku
