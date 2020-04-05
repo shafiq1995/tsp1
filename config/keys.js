04866bcb913a313de5c4da84bb97c10647b78ca1
shafiq1995 <shafiqshumon0@gmail.com>
2020-04-05 23:55:33 +0600

    05-04-2020

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://ssr:ssr0524@stp-ottoi.mongodb.net/test?retryWrites=true&w=majority',
+};
--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
 
+mongoose.connect(
+    db, {
+        useNewUrlParser: true
+    },
+    () => {
+        console.log("MongoDB is connected");
+    },
+).then(
+    () => {
+        app.get('/db', (req, res) => {
+            res.send({
+                Hey: 'Database is UP'
+            });
+        });
+    }
+);
 
 app.get('/', (req, res) => {
-    res.send({Bye: 'There'});
+    res.send({
+        Hey: 'There'
+    });
 });
 
 // To deploy on heroku
